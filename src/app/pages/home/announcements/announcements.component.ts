c0c56fa3b0dd66e14a63c843afdb6355ed59a48d
EagleMind <hassen.ben.mbarek@gmail.com>
2025-01-27 14:49:17 +0100

    updated login ui & announcements fixes

--- src/app/pages/home/announcements/announcements.component.ts
+++ src/app/pages/home/announcements/announcements.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AnnouncementDialogComponent } from './announcements-modal/modal.component';
 import { Subscription } from 'rxjs';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import moment from 'moment';
-import { ColDef, ColGroupDef, GridOptions } from 'ag-grid-community';
+import { ColDef, GridOptions } from 'ag-grid-community';
+
+import { AnnouncementDialogComponent } from './announcements-modal/modal.component';
 import {
   Announcement,
   AnnouncementsResponse,
@@ -21,53 +22,14 @@ import { ActionCellRendererComponent } from './render-button/render-buttons.comp
 })
 export class AnnouncementsComponent implements OnInit, OnDestroy {
   announcements: Announcement[] = [];
-  totalElements: number = 0;
-  currentPage: number = 0;
-  pageSize: number = 10;
+  totalElements = 0;
+  currentPage = 0;
+  pageSize = 10;
   gridApi: any;
-  @Input() announcementId: string = '';
-  faPen = faPen;
-  columnDefs: (ColDef<Announcement> | ColGroupDef<Announcement>)[] = [
-    {
-      headerName: 'Name',
-      headerClass: 'text-white',
-      field: 'name',
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: 'URL',
-      headerClass: 'text-white',
-      field: 'url',
-      sortable: true,
-      filter: true,
-    },
-    {
-      headerName: 'Date From',
-      headerClass: 'text-white',
-      field: 'dateFrom',
-      sortable: true,
-      filter: true,
-      valueFormatter: (params) => this.formatDate(params.value),
-    },
-    {
-      headerName: 'Date To',
-      headerClass: 'text-white',
-      field: 'dateTo',
-      sortable: true,
-      filter: true,
-      valueFormatter: (params) => this.formatDate(params.value),
-    },
-    {
-      headerName: 'Actions',
-      headerClass: 'text-white',
-      cellRenderer: ActionCellRendererComponent,
-      cellRendererParams: {
-        context: this,
-      },
-    },
-  ];
+  @Input() announcementId = '';
+  private subscriptions: Subscription[] = [];
 
+  columnDefs: ColDef<Announcement>[] = [];
   gridOptions: GridOptions = {
     pagination: true,
     paginationPageSize: this.pageSize,
@@ -76,21 +38,153 @@ export class AnnouncementsComponent implements OnInit, OnDestroy {
       sortable: true,
       filter: true,
     },
+    domLayout: 'autoHeight',
   };
-
-  private subscriptions: Subscription[] = [];
-
-  faTrash = faTrash;
 
   constructor(
     private dialog: MatDialog,
     private announcementService: AnnouncementService,
     private snackBar: MatSnackBar,
-    private eventBus: EventBusService
+    private eventBus: EventBusService,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
+    this.initializeGridColumns();
     this.loadAnnouncements();
+    this.manageEvents();
+
+    this.subscriptions.push(
+      this.breakpointObserver
+        .observe([Breakpoints.Small, Breakpoints.Handset])
+        .subscribe((state) => {
+          if (state.matches) {
+            this.adjustColumnsForSmallScreens();
+          } else {
+            this.initializeGridColumns();
+          }
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  private initializeGridColumns(): void {
+    this.columnDefs = [
+      {
+        headerName: 'Name',
+        field: 'name',
+        sortable: true,
+        filter: true,
+        flex: 2,
+      },
+      {
+        headerName: 'URL',
+        field: 'url',
+        sortable: true,
+        filter: true,
+        flex: 2,
+      },
+      {
+        headerName: 'Date From',
+        field: 'dateFrom',
+        sortable: true,
+        filter: true,
+        valueFormatter: (params) => this.formatDate(params.value),
+      },
+      {
+        headerName: 'Date To',
+        field: 'dateTo',
+        sortable: true,
+        filter: true,
+        valueFormatter: (params) => this.formatDate(params.value),
+      },
+      {
+        headerName: 'Actions',
+        cellRenderer: ActionCellRendererComponent,
+        cellRendererParams: {
+          context: this,
+        },
+      },
+    ];
+  }
+
+  private adjustColumnsForSmallScreens(): void {
+    this.columnDefs = this.columnDefs.map((col) => ({
+      ...col,
+      hide: col.field !== 'name', // Show only "Name" for smaller screens
+    }));
+  }
+
+  onGridReady(params: any): void {
+    this.gridApi = params.api;
+    this.loadAnnouncements();
+  }
+
+  loadAnnouncements(): void {
+    this.subscriptions.push(
+      this.announcementService
+        .getAllAnnouncements(this.currentPage, this.pageSize)
+        .subscribe({
+          next: (response: AnnouncementsResponse) => {
+            this.announcements = response.content;
+            this.totalElements = response.totalElements;
+          },
+          error: () => this.showError('Error loading announcements'),
+        })
+    );
+  }
+
+  openDialog(announcement?: Announcement): void {
+    const dialogRef = this.dialog.open(AnnouncementDialogComponent, {
+      width: '400px',
+      data: announcement || { name: '', url: '', dateFrom: '', dateTo: '' },
+    });
+
+    this.subscriptions.push(
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
+          result.id
+            ? this.updateAnnouncement(result)
+            : this.createAnnouncement(result);
+        }
+      })
+    );
+  }
+
+  private updateAnnouncement(updatedAnnouncement: Announcement): void {
+    this.subscriptions.push(
+      this.announcementService
+        .updateAnnouncement(updatedAnnouncement.id, updatedAnnouncement)
+        .subscribe({
+          next: () => {
+            this.loadAnnouncements();
+            this.snackBar.open('Announcement updated successfully', 'Close', {
+              duration: 3000,
+            });
+          },
+          error: () => this.showError('Error updating announcement'),
+        })
+    );
+  }
+
+  private createAnnouncement(newAnnouncement: FormData): void {
+    this.subscriptions.push(
+      this.announcementService.createAnnouncement(newAnnouncement).subscribe({
+        next: () => {
+          this.loadAnnouncements();
+          this.snackBar.open('Announcement created successfully', 'Close', {
+            duration: 3000,
+          });
+        },
+        error: () => this.showError('Error creating announcement'),
+      })
+    );
+  }
+
+  private manageEvents(): void {
     this.subscriptions.push(
       this.eventBus.events$.subscribe((event) => {
         if (event === 'announcement-change') {
@@ -100,90 +194,11 @@ export class AnnouncementsComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
-  }
-
-  onGridReady(params: any): void {
-    this.gridApi = params.api;
-    this.loadAnnouncements();
-  }
-
-  loadAnnouncements(): void {
-    const subscription = this.announcementService
-      .getAllAnnouncements(this.currentPage, this.pageSize)
-      .subscribe({
-        next: (response: AnnouncementsResponse) => {
-          this.announcements = response.content;
-          this.totalElements = response.totalElements;
-        },
-        error: () => this.showError('Error loading announcements'),
-      });
-    this.subscriptions.push(subscription);
-  }
-
-  formatDate(date: string | Date, format: string = 'MMMM Do YYYY'): string {
+  private formatDate(
+    date: string | Date,
+    format: string = 'MMMM Do YYYY'
+  ): string {
     return moment(date).format(format);
-  }
-
-  openDialog(announcement?: Announcement): void {
-    const dialogRef = this.dialog.open(AnnouncementDialogComponent, {
-      width: '400px',
-      data: announcement || { name: '', url: '', dateFrom: '', dateTo: '' },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        result.id
-          ? this.updateAnnouncement(result)
-          : this.createAnnouncement(result);
-      }
-    });
-  }
-
-  updateAnnouncement(updatedAnnouncement: Announcement): void {
-    const subscription = this.announcementService
-      .updateAnnouncement(updatedAnnouncement.id, updatedAnnouncement)
-      .subscribe({
-        next: () => {
-          this.loadAnnouncements();
-          this.snackBar.open('Announcement updated successfully', 'Close', {
-            duration: 3000,
-          });
-        },
-        error: () => this.showError('Error updating announcement'),
-      });
-    this.subscriptions.push(subscription);
-  }
-
-  createAnnouncement(newAnnouncement: FormData): void {
-    const subscription = this.announcementService
-      .createAnnouncement(newAnnouncement)
-      .subscribe({
-        next: () => {
-          this.loadAnnouncements();
-          this.snackBar.open('Announcement created successfully', 'Close', {
-            duration: 3000,
-          });
-        },
-        error: () => this.showError('Error creating announcement'),
-      });
-    this.subscriptions.push(subscription);
-  }
-
-  deleteAnnouncement(): void {
-    const subscription = this.announcementService
-      .deleteAnnouncement(this.announcementId)
-      .subscribe({
-        next: () => {
-          this.loadAnnouncements();
-          this.snackBar.open('Announcement deleted successfully', 'Close', {
-            duration: 3000,
-          });
-        },
-        error: () => this.showError('Error deleting announcement'),
-      });
-    this.subscriptions.push(subscription);
   }
 
   private showError(message: string): void {
