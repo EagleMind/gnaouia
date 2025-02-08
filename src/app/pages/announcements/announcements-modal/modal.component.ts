486fabcbeda09b72c4b298e0a4a0bfc57ab94e9d
EagleMind <hassen.ben.mbarek@gmail.com>
2025-02-08 11:08:14 +0100

    modal fixes

--- src/app/pages/announcements/announcements-modal/modal.component.ts
+++ src/app/pages/announcements/announcements-modal/modal.component.ts
@@ -1,5 +1,6 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Inject,
   Input,
@@ -24,13 +25,19 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+
 import {
   FaIconLibrary,
   FontAwesomeModule,
 } from '@fortawesome/angular-fontawesome';
-import { faPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faCheck,
+  faXmarkCircle,
+} from '@fortawesome/free-solid-svg-icons';
 import { EventBusService } from '../../../_services/event-bus.service';
 import { AnnouncementService } from '../../../_services/announcements';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-announcement-dialog',
@@ -56,25 +63,33 @@ import { AnnouncementService } from '../../../_services/announcements';
 export class AnnouncementDialogComponent implements OnInit {
   faPlus = faPlus;
   faCheck = faCheck;
+  faXmarkCircle = faXmarkCircle;
   selectedFile: File | null = null;
   form: FormGroup;
   isEditMode: boolean = false;
   isDeleteMode: boolean = false;
-
+  isLoading = true;
   constructor(
+    private snackBar: MatSnackBar,
     private dateAdapter: DateAdapter<Date>,
     private eventBusService: EventBusService,
     private announcementService: AnnouncementService,
     public dialogRef: MatDialogRef<AnnouncementDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private cdRef: ChangeDetectorRef
   ) {
     this.dateAdapter.setLocale('en-GB');
     this.form = new FormGroup({
       name: new FormControl(data?.name || '', [Validators.required]),
-      url: new FormControl(data?.url || '', [Validators.required]),
-      dateFrom: new FormControl(data?.dateFrom || null),
-      dateTo: new FormControl(data?.dateTo || null),
-      pictureUrl: new FormControl(data?.pictureUrl || null),
+      url: new FormControl(data?.url || '', [
+        Validators.required,
+        Validators.pattern('https?://.+'),
+      ]),
+      dateFrom: new FormControl(data?.dateFrom || null, [Validators.required]),
+      dateTo: new FormControl(data?.dateTo || null, [Validators.required]),
+      pictureUrl: new FormControl(data?.pictureUrl || null, [
+        Validators.required,
+      ]),
     });
     this.isEditMode = !!data?.id; // Set edit mode if ID exists
     this.isDeleteMode = data?.isDelete; // Check if it's for deletion
@@ -92,8 +107,9 @@ export class AnnouncementDialogComponent implements OnInit {
 
       reader.onload = (e: any) => {
         this.data.pictureUrl = e.target.result;
+        this.form.patchValue({ pictureUrl: e.target.result });
+        this.cdRef.detectChanges();
       };
-
       reader.readAsDataURL(this.selectedFile);
     }
   }
@@ -114,10 +130,12 @@ export class AnnouncementDialogComponent implements OnInit {
     const formData = this.prepareFormData();
     this.announcementService.createAnnouncement(formData).subscribe({
       next: () => {
+        this.showSnackbar('Announcement created successfully', 'success');
         this.dialogRef.close(true);
         this.eventBusService.emit('announcement-change');
       },
-      error: (err) => console.error('Failed to create announcement', err),
+      error: () => this.showSnackbar('Failed to create announcement', 'error'),
+      complete: () => (this.isLoading = false),
     });
   }
 
@@ -128,20 +146,25 @@ export class AnnouncementDialogComponent implements OnInit {
       .updateAnnouncement(this.data.id, formData)
       .subscribe({
         next: () => {
+          this.showSnackbar('Announcement created successfully', 'success');
           this.dialogRef.close(true);
           this.eventBusService.emit('announcement-change');
         },
-        error: (err) => console.error('Failed to update announcement', err),
+        error: () =>
+          this.showSnackbar('Failed to create announcement', 'error'),
+        complete: () => (this.isLoading = false),
       });
   }
 
   private deleteAnnouncement(): void {
     this.announcementService.deleteAnnouncement(this.data.id).subscribe({
       next: () => {
+        this.showSnackbar('Announcement Deleted successfully', 'success');
         this.dialogRef.close(true);
         this.eventBusService.emit('announcement-change');
       },
-      error: (err) => console.error('Failed to delete announcement', err),
+      error: () => this.showSnackbar('Failed to Delete announcement', 'error'),
+      complete: () => (this.isLoading = false),
     });
   }
 
@@ -161,5 +184,11 @@ export class AnnouncementDialogComponent implements OnInit {
 
   private formatDate(date: string): string {
     return date ? moment(this.form.value.dateFrom).format('L') : '';
+  }
+  private showSnackbar(message: string, type: 'success' | 'error'): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      panelClass: type === 'success' ? 'snackbar-success' : 'snackbar-error',
+    });
   }
 }
